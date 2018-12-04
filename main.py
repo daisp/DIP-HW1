94cac0c7f99e7bd329fe259e12f032dd3ff0e564
nevo <nevoagmon@gmail.com>
2018-12-04 19:30:19 +0200

    added plt

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import scipy.io as sio
+from pathlib import Path
+import matplotlib.pyplot as plt
 
+
+def main():
+    mat = sio.loadmat(Path('./hw1/100_motion_paths.mat'))
+    x_mat = mat['X']
+    y_mat = mat['Y']
+    for traj in zip(x_mat, y_mat):
+        plt.plot(*traj)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
