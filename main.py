e172df27de030cc0e3aa101e711cf6ce87af6690
Dais <daispaysagist@gmail.com>
2018-12-17 21:33:37 +0200

    conv2d creates images with huge pixel values.

--- main.py
+++ main.py
@@ -12,43 +12,57 @@ PSF_SIZE = 7
 
 
 def blur_images_and_show(image, show=True, stop=0):
+    print(image)
     mat = sio.loadmat(Path('./hw1/100_motion_paths.mat'))
     x_mat = mat['X']
     y_mat = mat['Y']
-    blured_images = []
+    blurred_images = []
     i = 0
     for traj in zip(x_mat, y_mat):
         i += 1
-        plt.figure()
         x, y = traj
-        plt.plot(y, x)
-        plt.gca().invert_yaxis()
-        psf = generate_psf(*traj)
-        plt.figure()
-        plt.imshow(psf, cmap='gray')
-        plt.figure()
-        blurred_image = conv2d(image, psf, 'same')
-        blured_images.append(blurred_image)
-        plt.imshow(blurred_image, cmap='gray')
         if show:
+            plt.figure()
+            plt.plot(y, x)
+            plt.gca().invert_yaxis()
+        psf = generate_psf(*traj)
+        if show:
+            plt.figure()
+            plt.imshow(psf, cmap='gray')
+        blurred_image = conv2d(image, psf, 'same')
+        print(blurred_image)
+        blurred_images.append(blurred_image)
+        # print(blurred_image)
+        if show:
+            plt.figure()
+            plt.imshow(blurred_image, cmap='gray')
             plt.show()
         if i == stop:
             break
-    return blured_images
+    return blurred_images
 
 
-def restore_image(images, origin_image):
-    psnr = lambda orig_img, img2: 10 * math.log10(np.amax(orig_img) ** 2) / np.mean((orig_img - img2) ** 2)
+def restore_image(images, origin_image, show=True):
+    psnr = lambda orig_img, img2: 10 * math.log10((255 ** 2) / np.mean((orig_img - img2) ** 2))
     psnr_list = []
     fba_image = None
     for image in images:
         if fba_image is None:
             fba_image = fft2(image)
         else:
-            fba_image = np.maximum(np.abs(fba_image), np.abs(fft2(image)))
-        ifft_ = np.abs(ifft2(fba_image))
-        n = psnr(origin_image, ifft_)
-        print(n)
+            image_ft = fft2(image)
+            for row in range(256):
+                for col in range(256):
+                    if np.abs(fba_image[row, col]) < np.abs(image_ft[row, col]):
+                        fba_image[row, col] = image_ft[row, col]
+        restored_image = np.abs(ifft2(fba_image))
+        # print(image)
+        if show:
+            plt.figure()
+            plt.imshow(restored_image, cmap='gray')
+            plt.show()
+        n = psnr(origin_image, restored_image)
+        # print(n)
         psnr_list.append(n)
     return psnr_list
 
@@ -59,15 +73,14 @@ def generate_psf(cont_x, cont_y):
     discrete = np.round(discrete)
     psf = np.zeros((PSF_SIZE, PSF_SIZE), dtype=int)
     for x, y in zip(discrete[0], discrete[1]):
-        psf[int(x)+PSF_SIZE//2, int(y)+PSF_SIZE//2] += 1
+        psf[int(x) + PSF_SIZE // 2, int(y) + PSF_SIZE // 2] += 1
     return psf
 
 
 if __name__ == '__main__':
-    image = cv2.imread(str(Path('./hw1/DIPSourceHW1.jpg')), 0)
-    images = blur_images_and_show(image, show=False, stop=5)
-    # print(len(images))
-    res = restore_image(images, image)
+    original_image = cv2.imread(str(Path('./hw1/DIPSourceHW1.jpg')), 0)
+    blurred_images = blur_images_and_show(original_image, show=False, stop=2)
+    res = restore_image(blurred_images, original_image, show=False)
     plt.figure()
     plt.plot(res)
     plt.show()
