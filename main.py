670b9ffa906c3020aab200e7abdad48beeffa187
nevo <nevoagmon@gmail.com>
2018-12-20 13:12:22 +0200

    code cleanup & improved calclation by p

--- main.py
+++ main.py
@@ -8,39 +8,48 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
-PSF_SIZE = 7
+RES_PATH = './hw1/results'
 
-
-def blur_images_and_show(image, show=True, stop=0):
-    # print(image)
+def blur_images_and_show(image, psf_size=19, show=True, stop=0, random_plot_size=0, save_to_file=False):
     mat = sio.loadmat(Path('./hw1/100_motion_paths.mat'))
     x_mat = mat['X']
     y_mat = mat['Y']
     blurred_images = []
     i = 0
+    random_plot = lambda i: i in [int(x) for x in np.round(np.random.rand(random_plot_size) * len(x_mat))]
     for traj in zip(x_mat, y_mat):
         i += 1
         x, y = traj
-        if show:
+        if show or random_plot(i) or save_to_file:
             plt.figure()
+            plt.title(f'trajectory #{i}')
             plt.plot(y, x)
             plt.gca().invert_yaxis()
-        psf = generate_psf(*traj)
-        if show:
+            if save_to_file:
+                plt.savefig(Path(RES_PATH + f'trajectory_{i}.png'), bbox_inches='tight')
+        psf = generate_psf(psf_size, *traj)
+        if show or random_plot(i) or save_to_file:
             plt.figure()
+            plt.title(f'PSF for trajectory #{i}')
             plt.imshow(psf, cmap='gray')
+            if save_to_file:
+                plt.savefig(Path(RES_PATH + f'PSF_for_trajectory_{i}.png'), bbox_inches='tight')
         blurred_image = conv2d(image, psf, 'same')
         blurred_images.append(blurred_image)
-        if show:
+        if show or random_plot(i) or save_to_file:
             plt.figure()
+            plt.title(f'blured image #{i}')
             plt.imshow(blurred_image, cmap='gray')
-            plt.show()
+            if save_to_file:
+                plt.savefig(Path(RES_PATH + f'blured_image_{i}.png'), bbox_inches='tight')
+            if show:
+                plt.show()
         if i == stop:
             break
     return blurred_images
 
 
-def restore_image(images, origin_image, show=True, p=np.inf):
+def restore_image(images, origin_image, show=True, p=np.inf, save_to_file=False):
     psnr = lambda orig_img, img2: 10 * math.log10((255 ** 2) / np.mean(np.power(orig_img - img2, 2)))
     psnr_list = []
     restored_img = None
@@ -48,10 +57,14 @@ def restore_image(images, origin_image, show=True, p=np.inf):
         restored_img = compute_by_max(images, restored_img, psnr, psnr_list, origin_image)
     else:
         restored_img = compute_by_p(images, p, restored_img, psnr, psnr_list, origin_image)
-    if show:
+    if show or save_to_file:
         plt.figure()
+        plt.title('restored image')
         plt.imshow(restored_img, cmap='gray')
-        plt.show()
+        if save_to_file:
+            plt.savefig(Path(RES_PATH + 'restored_image.png'), bbox_inches='tight')
+        if show:
+            plt.show()
     return psnr_list, restored_img
 
 
@@ -92,27 +105,48 @@ def compute_by_max(images, restored_img, psnr, psnr_list, origin_image):
     return restored_img
 
 
-def generate_psf(cont_x, cont_y):
+def generate_psf(psf_size, cont_x, cont_y):
+    # print(f'current psf size is {psf_size}')
     indxs = np.linspace(0, 1001, num=256, endpoint=False, dtype=int)
     discrete = np.array((cont_x[indxs], cont_y[indxs]))
-    discrete[0] /= np.max(np.abs(discrete[0]))
-    discrete[1] /= np.max(np.abs(discrete[1]))
-    discrete *= (PSF_SIZE - 1) // 2
+    # np_max = np.max(np.abs(discrete[0]))
+    # print(np_max)
+    # discrete[0] /= np_max
+    # n = np.max(np.abs(discrete[1]))
+    # print(n)
+    # discrete[1] /= n
+    # discrete *= (psf_size - 1) // 2
     discrete = np.round(discrete)
-    psf = np.zeros((PSF_SIZE, PSF_SIZE), dtype=int)
+    # print(np.max(discrete))
+    # print(discrete)
+    psf = np.zeros((psf_size, psf_size), dtype=int)
     for x, y in zip(discrete[0], discrete[1]):
-        psf[int(x) + (PSF_SIZE - 1) // 2, int(y) + (PSF_SIZE - 1) // 2] += 1
+        psf[int(x) + (psf_size - 1) // 2, int(y) + (psf_size - 1) // 2] += 1
     return psf / np.sum(psf)
+
+
+def show_restoration_results(p_val=np.inf):
+    psnr_list_results, restored_image = restore_image(blurred_images, original_image, show=False, p=p_val)
+    print(f'max psnr for p:{p_val} is {max(psnr_list_results)}')
+    plt.figure()
+    plt.plot(psnr_list_results)
+    plt.title(f'psnr comparison with p={p_val}')
+    plt.xlabel('# images')
+    plt.ylabel('PSNR value')
+    plt.savefig(Path(RES_PATH + f'psnr_comparison.png'), bbox_inches='tight')
+    plt.figure()
+    plt.title('restored_image')
+    plt.imshow(restored_image, cmap='gray')
+    plt.savefig(Path(RES_PATH + 'restored_image.png'), bbox_inches='tight')
 
 
 if __name__ == '__main__':
     original_image = cv2.imread(str(Path('./hw1/DIPSourceHW1.jpg')), 0)
-    blurred_images = blur_images_and_show(original_image, show=False, stop=0)
-    psnr_list_results, restored_image = restore_image(blurred_images, original_image, show=False, p=20)
-    plt.figure()
-    plt.plot(psnr_list_results)
-    plt.figure()
-    plt.imshow(original_image, cmap='gray')
-    plt.figure()
-    plt.imshow(restored_image, cmap='gray')
+    blurred_images = blur_images_and_show(original_image, psf_size=19, show=False, stop=0, random_plot_size=0)
+    # ====== test best p ========#
+    # show_restoration_results()
+    # for p in range(10, 25):
+    #     show_restoration_results(p)
+    # ===========================#
+    show_restoration_results(11)
     plt.show()
